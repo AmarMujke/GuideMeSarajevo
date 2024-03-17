a80d8e5b41735c2d3d1e3196600fcc4736663bce
MujkicAmar <a.mujkic@devla.dev>
2024-03-18 00:58:47 +0100

    Initialization of the backend and frontend, spring&react

---
+++ src/main/java/GuideMeSarajevocom/example/GuideMeSarajevocom/GuideMeSarajevocomApplication.java
@@ -0,0 +1,13 @@
+package GuideMeSarajevocom.example.GuideMeSarajevocom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideMeSarajevocomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideMeSarajevocomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GuideMeSarajevocom/example/GuideMeSarajevocom/GuideMeSarajevocomApplicationTests.java
@@ -0,0 +1,13 @@
+package GuideMeSarajevocom.example.GuideMeSarajevocom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideMeSarajevocomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
