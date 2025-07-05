c8c65a1be657fd6b27092305f13d5fd57b32879e
AmarMujke <58230291+AmarMujke@users.noreply.github.com>
2025-07-05 14:52:49 +0200

    Update WebConfig.java

--- src/main/java/GuideMeSarajevocom/example/GuideMeSarajevocom/config/WebConfig.java
+++ src/main/java/GuideMeSarajevocom/example/GuideMeSarajevocom/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173",
+                        "https://guidemesarajevo-2.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
