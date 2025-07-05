824dd4c71312202224f430c5c0e3539f6ae39001
MujkicAmar <amar.mujka@outlook.com>
2025-07-05 14:06:29 +0200

    vite config fix

--- Front/GuideMeSarajevo/vite.config.js
+++ Front/GuideMeSarajevo/vite.config.js
@@ -1,16 +1,23 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL, 
-        changeOrigin: true,
-        secure: false,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, __dirname, '');
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-});
+  });
+};
