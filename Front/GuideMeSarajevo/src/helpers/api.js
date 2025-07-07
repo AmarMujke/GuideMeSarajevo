ee909d1df57d1bc0d519ac190a4db57fdc123b0e
MujkicAmar <amar.mujka@outlook.com>
2025-07-07 23:59:42 +0200

    new backend url

--- Front/GuideMeSarajevo/src/helpers/api.js
+++ Front/GuideMeSarajevo/src/helpers/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://guidemesarajevo.onrender.com",
+  baseURL: "https://guidemesarajevo-1.onrender.com",
 });
 
 api.interceptors.request.use(
